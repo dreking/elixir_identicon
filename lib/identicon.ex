5a98f95cd6b6a5410b22c35c8ae6c1b755bcd28b
Leandre Nsengumuremyi <leajapo@gmail.com>
2026-04-10 15:52:33 +0200

    feat: added grid and map function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -16,6 +16,7 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
   end
 
   def hash_input(input) do
@@ -29,5 +30,21 @@ defmodule Identicon do
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     # %Identicon.Image{hex: [r, g, b | _tail]} = image
     %Identicon.Image{(%Identicon.Image{} = image) | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3)
+    |> Enum.map(&mirrow_row/1)
+
+    # |> mirrow_row()
+  end
+
+  def mirrow_row(row) do
+    # 1, 2, 3
+    [first, second | _tail] = row
+
+    # 1, 2, 3, 2, 1
+    row ++ [second, first]
   end
 end
